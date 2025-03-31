b7f319ffe7fdb53d695b539cdeda84d80a799b73
Simon Baird <sbaird@redhat.com>
2025-03-31 08:26:24 -0400

    Support older pipelines without build-image-index
    If the build-image-index task doesn't exist, instead use the
    build-container task.
    
    Ref: https://issues.redhat.com/browse/EC-1202

--- spec/pipeline-patcher_spec.sh
+++ spec/pipeline-patcher_spec.sh
@@ -95,3 +95,45 @@ Adding task sast-unicode-check-oci-ta to pipeline $tmp_dir/.tekton/cli-v06-push.
     # Todo maybe: Make a baseline file and check that it matches exactly
   End
 End
+
+Describe 'add-tasks-with-build-container'
+  # Prepare a git repo for testing
+  # Todo: Move this into a helper..?
+  tmp_dir=$(mktemp -d)
+  trap "rm -rf $tmp_dir" EXIT
+  (
+    example_from_github="https://raw.githubusercontent.com/konflux-ci/mintmaker-renovate-image/9ca00ff4e7d87cfb768a2b6eff0185e9b8c8745f"
+    cd $tmp_dir && git init . && mkdir .tekton && cd .tekton
+    for y in push pull-request; do curl -sLO $example_from_github/.tekton/mintmaker-renovate-image-$y.yaml; done
+    git add * && git commit -m "Testing"
+  ) > /dev/null
+
+  show_diff() {
+    cd $tmp_dir && git diff
+  }
+
+  It 'produces expected output'
+    # Beware this pulls data from both github and quay
+    When run ./pipeline-patcher add-tasks $tmp_dir sast-shell-check,sast-unicode-check
+    The status should be success
+    The output should equal "Adding task sast-shell-check to pipeline $tmp_dir/.tekton/mintmaker-renovate-image-pull-request.yaml
+Adding task sast-unicode-check to pipeline $tmp_dir/.tekton/mintmaker-renovate-image-pull-request.yaml
+Adding task sast-shell-check to pipeline $tmp_dir/.tekton/mintmaker-renovate-image-push.yaml
+Adding task sast-unicode-check to pipeline $tmp_dir/.tekton/mintmaker-renovate-image-push.yaml
+ .tekton/mintmaker-renovate-image-pull-request.yaml | 48 ++++++++++++++++++++++
+ .tekton/mintmaker-renovate-image-push.yaml         | 48 ++++++++++++++++++++++
+ 2 files changed, 96 insertions(+)"
+    The value "$(show_diff)" should include "+    - name: sast-shell-check"
+    The value "$(show_diff)" should include "+            value: quay.io/konflux-ci/tekton-catalog/task-sast-shell-check:0.1@sha256"
+    The value "$(show_diff)" should include "+    - name: sast-unicode-check"
+    The value "$(show_diff)" should include "+            value: quay.io/konflux-ci/tekton-catalog/task-sast-unicode-check:0.1@sha256"
+
+    # Check that build-image-index was replaced with build-container
+    The value "$(show_diff)" should not include 'build-image-index'
+    The value "$(show_diff)" should include '+          value: $(tasks.build-container.results.IMAGE_URL)'
+    The value "$(show_diff)" should include '+          value: $(tasks.build-container.results.IMAGE_DIGEST)'
+    The value "$(show_diff)" should include '+        - build-container'
+
+    # Todo maybe: Make a baseline file and check that it matches exactly
+  End
+End
